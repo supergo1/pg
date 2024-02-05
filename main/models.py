9ea37b778b75eb7cebcda92c0c47d1c03200816d
Muhammad <muhamamdmamajonov@gmail.com>
2024-02-05 14:13:16 +0500

    a

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    age = models.IntegerField()
+    phone_number = models.CharField(max_length=13)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
