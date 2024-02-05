71085a2191fb39e909f423389008492785877092
ZaztyHokage <zazty2200@gmail.com>
2024-02-05 14:11:07 +0500

    a

--- main/models.py
+++ main/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255, blank=True, null=True)
+    age = models.IntegerField()
+    birth = models.DateField()
+    email = models.EmailField()
+    username = models.CharField(max_length=32, unique=True)
+    password = models.CharField(max_length=32)
+    created_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(first_name)
+
