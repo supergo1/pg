3945e2b75d513a983d0663a9714e379df4392418
ZaztyHokage <zazty2200@gmail.com>
2024-02-07 13:47:47 +0500

    a

--- main/models.py
+++ main/models.py
@@ -2,20 +2,6 @@ from django.db import models
 
 
 class User(models.Model):
-<<<<<<< HEAD
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=255, blank=True, null=True)
-    age = models.IntegerField()
-    birth = models.DateField()
-    email = models.EmailField()
-    username = models.CharField(max_length=32, unique=True)
-    password = models.CharField(max_length=32)
-    created_date = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return str(first_name)
-
-=======
     first_name = models.CharField(max_length=32)
     last_name = models.CharField(max_length=32)
     age = models.IntegerField()
@@ -23,4 +9,3 @@ class User(models.Model):
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
->>>>>>> 9ea37b778b75eb7cebcda92c0c47d1c03200816d
--- main/views.py
+++ main/views.py
@@ -7,6 +7,11 @@ from .models import User
 def index(request):
     users = User.objects.all()
     page = request.GET.get('page')
-    paginator = Paginator(users, 4)
+    if request.method == 'POST':
+        pages = request.POST['paginat']
+    if pages is None:
+        paginator = Paginator(users, 4)
+    else:
+        paginator = Paginator(users, pages)
     users = paginator.get_page(page)
     return render(request, 'index.html', {'users': users})
